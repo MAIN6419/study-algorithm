b1a5321effab70aab95902041945be8a1ffee8ed
jongtae.nam <113427991+MAIN6419@users.noreply.github.com>
2023-07-24 18:17:33 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,25 +1,12 @@
 function solution(strings, n) {
-    let temp;
-    let isSorted = true;
-    for(let i=0; i<strings.length; i++) {
-        for(let j=0; j<strings.length - 1 - i; j++) {
-            if(strings[j][n] > strings[j+1][n]){
-                temp = strings[j+1];
-                strings[j+1] = strings[j];
-                strings[j] = temp;
-                isSorted = false;
-            } else if(strings[j][n]===strings[j+1][n]){
-                if(strings[j]>strings[j+1]){
-                temp = strings[j+1];
-                strings[j+1] = strings[j];
-                strings[j] = temp;
-                isSorted = false;
-                }
-            }
+    return strings.sort((a, b) => {
+        const chr1 = a.charAt(n);
+        const chr2 = b.charAt(n);
+
+        if (chr1 == chr2) {
+            return a > b ? 1 : -1
+        } else {
+            return chr1 > chr2 ? 1 : -1
         }
-    }
-    if(isSorted) {
-        return strings;
-    }
-    return strings
+    })
 }
