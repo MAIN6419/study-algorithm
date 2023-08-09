9f8bc8ce5bdbe1ee2cdecade7fbc2119cfe20fba
jongtae.nam <113427991+MAIN6419@users.noreply.github.com>
2023-08-09 18:00:13 +0900

    [level 2] Title: 전화번호 목록, Time: 200.20 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,5 +1,20 @@
-
 function solution(phone_book) {
- const myMap = new Map();
-    myMap.set = 
+    const myMap = new Map();
+
+    // 전화번호를 Map에 저장
+    for (const phoneNumber of phone_book) {
+        myMap.set(phoneNumber, true);
+    }
+    
+    // 각 전화번호를 접두어로 갖는지 검사
+    for (const phoneNumber of phone_book) {
+        for (let i = 1; i < phoneNumber.length; i++) {
+            const sliceNum = phoneNumber.slice(0, i);
+            if (myMap.has(sliceNum)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
