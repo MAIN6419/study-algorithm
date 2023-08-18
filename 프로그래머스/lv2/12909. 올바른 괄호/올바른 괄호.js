d755594f2c0fc3f8dfc2c6be0dc7b0aa6fb48ca9
jongtae.nam <113427991+MAIN6419@users.noreply.github.com>
2023-08-18 23:06:37 +0900

    [level 2] Title: 올바른 괄호, Time: 3.99 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -14,3 +14,22 @@ function solution(s) {
     
     return answer.length === 0; // 스택에 남은 괄호가 없어야 올바른 괄호
 }
+// 테스트 1
+// "()()"
+// 1. "(" => o answer ['(']
+// 2. ")" => x answer []
+// 3. "(" => o answer ['(']
+// 4. ")" => x answer []
+// answer.length === 0 => true
+
+// 테스트 3
+// ")()("
+// 1. ")" => x answer.length===0 => false
+
+// 테스트 4
+// "(()("
+// 1. "(" => o answer ['(']
+// 2. "(" => o answer ['(', '(']
+// 3. ")" => x answer ['(']
+// 4. "(" => 0 answer ['(', '('] 
+// answer.length === 0 => false
